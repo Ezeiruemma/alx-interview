ffc9dfb6268ce28bc679646ad9f293623b1c1d80
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-07-06 02:23:42 +0100

    pascal trangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""
-Create a function def pascal_triangle(n)
-Returns a list of lists of integers
-representing the Pascal's triangle of (n)
 
-Returns an empty list if n <= 0
-You can assume n will be always an integer
+"""
+Pascal triangle
 """
 
 
 def pascal_triangle(n):
     """
-    return empty list if n <= 0
+    Returns a list of lists of integers representing
+    the Pascal’s triangle of n
+
     """
-    if n <= 0:
-        return []
+    pascal_triangle = [[1]]
 
-    a = [[] for i in range(n)]
+    if int(n) <= 0:
+        return list()
 
-    for i in range(n):
-        for j in range(i + 1):
-            if (j < i):
-                if (j == 0):
-                    a[i].append(1)
+    for i in range(n - 1):
+        if len(pascal_triangle[-1]) > 1:
+            last_item = pascal_triangle[-1]
+            triangle = [1]
+            for index, j in enumerate(last_item):
+                if index != (len(last_item) - 1):
+                    triangle.append(j + last_item[index + 1])
                 else:
-                    a[i].append(a[i - 1][j] + a[i - 1][j - 1])
-            elif (j == i):
-                a[i].append(1)
-    return a
+                    break
+            triangle.append(1)
+            pascal_triangle.append(triangle)
+        else:
+            pascal_triangle.append([1, 1])
+    return pascal_triangle
